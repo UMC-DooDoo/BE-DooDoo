9b5821bb5951771cf64bc08e8a10e7ec54e2c9e3
Yujin Kim <yujii22001@gmail.com>
2026-07-10 11:17:52 +0900

    feat: DB setting

---
+++ src/main/java/com/umc/doodoo/DoodooApplication.java
@@ -0,0 +1,13 @@
+package com.umc.doodoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc/doodoo/DoodooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.doodoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
