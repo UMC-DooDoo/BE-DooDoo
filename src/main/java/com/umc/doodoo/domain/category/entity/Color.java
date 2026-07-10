1db18364b83f23110278a8f3222f97923b7aea0a
Min Jiwon <minwldnjs87@gmail.com>
2026-07-10 17:07:30 +0900

    fix: 색상(Color) 값 프론트 최종안 반영

--- src/main/java/com/umc/doodoo/domain/category/entity/Color.java
+++ src/main/java/com/umc/doodoo/domain/category/entity/Color.java
@@ -7,20 +7,23 @@ import lombok.Getter;
 import java.util.Arrays;
 
 @Getter
-public enum
-    Color {
+public enum Color {
 
-    GREEN("GREEN"),
-    PINK("PINK"),
-    ORANGE("ORANGE"),
-    YELLOW("YELLOW"),
-    PURPLE("PURPLE"),
-    SKYBLUE("SKYBLUE");
+    BLUE("BLUE", "#1240D9"),
+    GREEN("GREEN", "#29C878"),
+    ORANGE("ORANGE", "#DC710E"),
+    PURPLE("PURPLE", "#7C2BE5"),
+    PINK("PINK", "#DC0E7C"),
+    CYAN("CYAN", "#2BC0E5"),
+    YELLOW("YELLOW", "#969B06"),
+    RED("RED", "#FF3241");
 
     private final String value;
+    private final String hexCode;
 
-    Color(String value) {
+    Color(String value, String hexCode) {
         this.value = value;
+        this.hexCode = hexCode;
     }
 
     public static Color fromValue(String value) {
